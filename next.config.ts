8f96f4747632444ac158fb0b8af022c73a1b3cbb
nilton667 <nilton667@gmail.com>
2024-10-28 21:29:02 +0100

    0.0.5

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
 
-const nextConfig: NextConfig = {
-  output: 'export',
-};
-
-export default nextConfig;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (phase: string) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  const nextConfig: NextConfig = {
+    output: 'export',
+    distDir: 'docs',
+    assetPrefix: isDev ? undefined : 'https://nilton667.github.io', 
+  }; 
+  
+  return nextConfig;
+}
